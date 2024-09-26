6c2114f151463d6298333cc64b2c3cd5174e54cd
AAA <1148696639@qq.com>
2024-09-26 17:14:32 +0800

    类工具类ClassHelper中添加复制变量的方法

--- DanmuFramework/Assets/Scripts/Utility/ClassHelper.cs
+++ DanmuFramework/Assets/Scripts/Utility/ClassHelper.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 public class ClassHelper
 {
     /// <summary>
@@ -14,7 +16,7 @@ public class ClassHelper
 
         var type = source.GetType();
         var properties = type.GetProperties();
- 
+
         foreach (var property in properties)
         {
             if (property.CanRead && property.CanWrite)
@@ -23,6 +25,30 @@ public class ClassHelper
                 property.SetValue(destination, value);
             }
         }
-        
+
+    }
+
+
+    /// <summary>
+    ///     定义一个方法来复制成员变量的值
+    /// </summary>
+    /// <param name="source">被复制的对象</param>
+    /// <param name="destination">目标对象</param>
+    public static void CopyFields(object source, object destination)
+    {
+        if (source.GetType() != destination.GetType())
+        {
+            DebugCtrl.LogError("无法复制不同的类");
+            return;
+        }
+
+        var type = source.GetType();
+        var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (var field in fields)
+        {
+            var value = field.GetValue(source);
+            field.SetValue(destination, value);
+        }
     }
 }
